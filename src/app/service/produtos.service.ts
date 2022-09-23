4a3c4e56a9ac85a5781673ce7aca051e158ee44e
Nataly Carvalho <natalycarvalho.dev@gmail.com>
2022-09-23 10:09:09 -0300

    Update produtos.service.ts

--- src/app/service/produtos.service.ts
+++ src/app/service/produtos.service.ts
@@ -16,23 +16,23 @@ export class ProdutosService {
   }
 
   getAllProdutos():Observable<Produtos[]>{
-    return this.http.get<Produtos[]>('https://agua-limpa.netlify.app/produtos', this.token)
+    return this.http.get<Produtos[]>('https://agualimpa.herokuapp.com/produtos', this.token)
 
   }
   getByIdProdutos(id: number): Observable<Produtos>{
-    return this.http.get<Produtos>(`https://agua-limpa.netlify.app/produtos/${id}`, this.token)
+    return this.http.get<Produtos>(`https://agualimpa.herokuapp.com/produtos/${id}`, this.token)
   }
 
   postProdutos(produtos: Produtos): Observable<Produtos>{
-    return this.http.post<Produtos>('https://agua-limpa.netlify.app/produtos', produtos, this.token)
+    return this.http.post<Produtos>('https://agualimpa.herokuapp.com/produtos', produtos, this.token)
   }
   putProdutos(produtos: Produtos): Observable<Produtos>{
-    return this.http.put<Produtos>('https://agua-limpa.netlify.app/produtos', produtos, this.token)
+    return this.http.put<Produtos>('https://agualimpa.herokuapp.com/produtos', produtos, this.token)
   }
   getByNome(nome: string): Observable<Produtos[]>{
-    return this.http.get<Produtos[]>(`https://agua-limpa.netlify.app/produtos/nome/${nome}`, this.token)
+    return this.http.get<Produtos[]>(`https://agualimpa.herokuapp.com/produtos/nome/${nome}`, this.token)
   }
   deleteProduto(id:number){
-    return this.http.get<Produtos>(`https://agua-limpa.netlify.app/produtos/${id}`, this.token)
+    return this.http.get<Produtos>(`https://agualimpa.herokuapp.com/produtos/${id}`, this.token)
     }
 }
