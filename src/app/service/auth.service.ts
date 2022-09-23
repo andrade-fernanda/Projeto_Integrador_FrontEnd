9c609b3ccf57eaff714775af06b745619bfa2fd9
Aline Beltrão <alinebelltrao@gmail.com>
2022-09-23 09:52:37 -0300

    Update auth.service.ts

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -16,22 +16,22 @@ export class AuthService {
   ) { }
 
   entrar(userLogin: UserLogin): Observable<UserLogin> {
-    return this.http.post<UserLogin>('http://localhost:8080/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('https://agua-limpa.netlify.app/usuarios/logar', userLogin)
 
   }
 
   cadastrar(user: User): Observable<User> {
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
+    return this.http.post<User>('https://agua-limpa.netlify.app/usuarios/cadastrar', user)
 
   }
 
   getByIdUser(id: number): Observable<User>{
-    return this.http.get<User>(`http://localhost:8080/usuarios/${id}`)
+    return this.http.get<User>(`https://agua-limpa.netlify.app/usuarios/${id}`)
   }
 
 
   atualizar(user:User): Observable<User>{
-    return this.http.put<User>('http://localhost:8080/usuarios/atualizar', user)
+    return this.http.put<User>('https://agua-limpa.netlify.app/usuarios/atualizar', user)
   }
 
   
